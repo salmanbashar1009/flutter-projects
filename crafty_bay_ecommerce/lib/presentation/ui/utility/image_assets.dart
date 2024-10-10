7a00edb03c7b5b43c7839b814ecfa516ba15d4ab
salmanbashar1009 <salmanbashar101@gmail.com>
2024-10-10 14:16:05 +0600

    code updated

--- crafty_bay_ecommerce/lib/presentation/ui/utility/image_assets.dart
+++ crafty_bay_ecommerce/lib/presentation/ui/utility/image_assets.dart
@@ -3,6 +3,7 @@
 
 class ImagePathHolder{
   static const String canvasLogoSVG = "assets/images/canvas-logo.svg";
+  static const String logoNav = "assets/images/logo_nav.svg";
 
   static const String versionCode = 'Version 1.0.0';
 }
